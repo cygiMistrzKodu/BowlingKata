7077b15e74b165a6b29337859b0244473484c081
Jacek programmer <jacek301@gmail.com>
2024-02-06 21:14:43 +0100

    Bowling game degenerated 2 first two test

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,12 +1,41 @@
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
 
-    @Test
-    public void test() {
+    private Game g;
 
-        assert false;
+    @BeforeEach
+    public void setUp() {
+        g = new Game();
+    }
+
+    private void rollMany(int n,int pins) {
+        for (int i = 0; i < n; i++) {
+
+            g.roll(pins);
+        }
+    }
+
+    @Test
+    public void gutterGame() {
+
+        rollMany(20,0);
+        assertEquals(0, g.score());
+
+    }
+
+    @Test
+    public void allOnes() {
+
+        rollMany(20,1);
+        assertEquals(20, g.score());
 
     }
 
 }
+
+
